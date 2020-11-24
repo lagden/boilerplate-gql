076b7d4c9c152fff48ec55d3f9b2c2473b5cde9f
Thiago Lagden <lagden@gmail.com>
2020-11-24 17:48:35 -0300

    add lodash merge

--- server/schema/resolvers/index.js
+++ server/schema/resolvers/index.js
@@ -2,15 +2,14 @@
 
 const {readdirSync} = require('fs')
 const {join} = require('path')
-// const merge = require('lodash.merge')
+const merge = require('lodash.merge')
 
 const pattern = /^_[\w-_]+\.js/
 const files = readdirSync(__dirname).filter(f => pattern.test(f)).map(f => join(__dirname, f))
-let resolvers = {}
+const resolvers = {}
 
 for (const file of files) {
-	resolvers = {...resolvers, ...require(file)}
-	// merge(resolvers, require(file))
+	merge(resolvers, require(file))
 }
 
 module.exports = resolvers
