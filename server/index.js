45391e3b71691ad6b367c0bcb70bf1ec70c307ff
Thiago Lagden <lagden@gmail.com>
2021-03-05 00:58:54 -0300

    add local-access

--- server/index.js
+++ server/index.js
@@ -1,18 +1,25 @@
 'use strict'
 
+const la = require('local-access')
 const debug = require('./lib/debug')
 const app = require('./app')
 
 const {
 	PORT = 5000,
 	PORT_PUBLISHED = 5000,
+	HOST = '0.0.0.0',
 	VERSION = 'dev'
 } = process.env
+
+let {
+	local,
+	network
+} = la({PORT_PUBLISHED, HOST})
 
 app.listen(PORT, () => {
 	debug.info('Server listening')
 	debug.info('----------------')
-	debug.info(`Local:    http://[::1]:${PORT}`)
-	debug.info(`External: http://[::]:${PORT_PUBLISHED}`)
+	debug.info(`Local:    ${local}`)
+	debug.info(`Network:  ${network}`)
 	debug.info(`Version:  ${VERSION}`)
 })
